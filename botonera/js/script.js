b48e1370b9020e5e790251ecfe5ea3ad593121c4
Gonza <gonzarascon@gmail.com>
2018-07-11 15:42:15 -0300

    Path finding test

--- botonera/js/script.js
+++ botonera/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   $.getJSON('json/sounds.json', function(sound) {
       /*optional stuff to do after success */
       console.log(sound);
+      console.log(sound[0].path);
       $.each(sound,function(i, button) {
         sounds.push('<button id="sound-'+ i + '">' + sound + '</button>');
       });
