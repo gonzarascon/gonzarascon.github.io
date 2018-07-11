52ab7e3385f62d3bd496fb032c48150c2de7e395
Gonza <gonzarascon@gmail.com>
2018-07-11 15:38:42 -0300

    Json testing

--- botonera/js/script.js
+++ botonera/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
   $.getJSON('json/sounds.json', function(sound) {
       /*optional stuff to do after success */
+      console.log(sound);
       $.each(sound,function(i, button) {
         sounds.push('<button id="sound-'+ i + '">' + button + '</button>');
       });
