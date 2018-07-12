42568c23aa1cfc2de60cea7407986c4f54d5f8b1
Gonza <gonzarascon@gmail.com>
2018-07-11 23:35:30 -0300

    Works?

--- botonera/js/script.js
+++ botonera/js/script.js
@@ -6,20 +6,21 @@ $(document).ready(function() {
   $.getJSON('json/sounds.json', function(sound) {
       /*optional stuff to do after success */
       $.each(sound,function(i, button) {
-        soundPath.push("'https://gonzarascon.github.io/botonera/" + sound[i].path + "'");
+        soundPath.push("https://gonzarascon.github.io/botonera/" + sound[i].path );
         sounds.push('<button id="sound-'+ i + '" class = "sound" data-sound = "' + i + '">' + sound[i].name + '</button>');
       });
       $.each(sounds,function(i, element) {
         $('.button-grid').append(element);
       });
 
+      console.log(soundPath);
+      let soundButtons = new Howl({
+        src : soundPath,
+        html5 : true,
+        autoplay: false
+      });
   });
-  console.log(soundPath);
-  let soundButtons = new Howl({
-    src : soundPath,
-    html5 : true,
-    autoplay: false
-  });
+
 
   $('.button-grid').on('click','.sound', function(event) {
     event.preventDefault();
