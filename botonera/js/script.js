8c9a9e2f3fa5c6bb5600e851848379d9950b6bc6
Gonza <gonzarascon@gmail.com>
2018-07-13 17:32:06 -0300

    introducing ion sound

--- botonera/js/script.js
+++ botonera/js/script.js
@@ -6,28 +6,30 @@ $(document).ready(function() {
   $.getJSON('json/sounds.json', function(sound) {
       /*optional stuff to do after success */
       $.each(sound,function(i, button) {
-        soundPath.push("https://gonzarascon.github.io/botonera/" + sound[i].path );
+        // soundPath.push("https://gonzarascon.github.io/botonera/" + sound[i].path );
+        soundPath.push('{ name: "' + sound[i].file + '"}')
         sounds.push('<button id="sound-'+ i + '" class = "sound" data-sound = "' + i + '">' + sound[i].name + '</button>');
       });
       $.each(sounds,function(i, element) {
         $('.button-grid').append(element);
       });
 
+      // console.log(soundPath);
+      // soundButtons = new Howl({
+      //   src : soundPath,
+      //   html5 : true,
+      //   autoplay: false
+      // });
       console.log(soundPath);
-      soundButtons = new Howl({
-        src : soundPath,
-        html5 : true,
-        autoplay: false
-      });
   });
 
 
   $('.button-grid').on('click','.sound', function(event) {
     event.preventDefault();
-    soundButtons.stop();
+    // soundButtons.stop();
     let actualSound = $(this).attr('data-sound');
     console.log(actualSound);
-    soundButtons.play(2);
+    // soundButtons.play(2);
   });
 
 });
