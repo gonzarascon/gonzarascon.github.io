e24fdbf3f5ed3f62e2ba71c354de756a0d2f3f85
Gonza <gonzarascon@gmail.com>
2018-07-14 15:38:28 -0300

    Deleted Howler.js and add .stop(); to ion sounds

--- botonera/js/script.js
+++ botonera/js/script.js
@@ -4,9 +4,7 @@ let soundButtons;
 $(document).ready(function() {
 
   $.getJSON('json/sounds.json', function(sound) {
-      /*optional stuff to do after success */
       $.each(sound,function(i, button) {
-        // soundPath.push("https://gonzarascon.github.io/botonera/" + sound[i].path );
         soundPath.push({ name:  sound[i].file});
         sounds.push('<button id="sound-'+ i + '" class = "sound" data-sound = "'+ sound[i].file + '">' + sound[i].name + '</button>');
       });
@@ -16,29 +14,21 @@ $(document).ready(function() {
 
       ion.sound({
       sounds: soundPath,
-      // main config
       path: "https://gonzarascon.github.io/botonera/audios/",
       preload: true,
       multiplay: false
       });
 
-      // console.log(soundPath);
-      // soundButtons = new Howl({
-      //   src : soundPath,
-      //   html5 : true,
-      //   autoplay: false
-      // });
       console.log(soundPath);
   });
 
 
   $('.button-grid').on('click','.sound', function(event) {
     event.preventDefault();
-    // soundButtons.stop();
+    ion.sound.stop();
     let actualSound = $(this).attr('data-sound');
     console.log(actualSound);
     ion.sound.play(actualSound);
-    // soundButtons.play(2);
   });
 
 });
