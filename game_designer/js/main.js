6b6d6e7d0041fd25e600b468f98fcd6227e69f0b
Gonza <gonzarascon@gmail.com>
2018-12-06 22:24:11 -0300

    Starting Interaction

--- game_designer/js/main.js
+++ game_designer/js/main.js
@@ -1,8 +1,34 @@
-var options= {
+let options= {
 			strings: ['No son juegos,^1000', 'son experiencias.^1000'],
 			typeSpeed:75,
 			loop:true,
 			backSpeed:50
 		}
 
-var typed = new Typed('#titles', options);
+let typed = new Typed('#titles', options);
+
+
+$(document).ready(function() {
+
+	// =================== MEDAL CASE SETTINGS
+	let medalButton = $('#medal');
+	let medalModal = $('#medal-container');
+	let medalArrow = $('#close-medal');
+
+	medalButton.on('click', function(event) {
+		event.preventDefault();
+		if (medalModal.is(':visible')) {
+			medalModal.slideUp('300');
+		} else{
+			medalModal.slideDown('300').css('display', 'grid');
+		}
+	});
+
+	medalArrow.on('click', function(event) {
+		event.preventDefault();
+		medalModal.slideUp('300');
+	});
+
+
+
+});
