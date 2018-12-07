30748d7f27caf46845590b48790aad634ddf6f8c
Gonza <gonzarascon@gmail.com>
2018-12-07 16:32:16 -0300

    Starting with media queries and added badge "system"

---
+++ game_designer/js/badges.js
@@ -0,0 +1,70 @@
+var fiveSeen = new ldBar('#fiveSeen',{
+	"type": "fill",
+	"img": "assets/img/badges/five-seen.svg",
+	"img-size": "100,100" ,
+	"fill-background": "rgba(0,0,0,0)",
+	"max": "100"
+});
+var allSeen = new ldBar('#allSeen',{
+	"type": "fill",
+	"img": "assets/img/badges/all-seen.svg",
+	"img-size": "100,100" ,
+	"fill-background": "rgba(0,0,0,0)",
+	"max": "100"
+});
+var profileSeen = new ldBar('#profileSeen',{
+	"type": "fill",
+	"img": "assets/img/badges/profile-seen.svg",
+	"img-size": "100,100" ,
+	"fill-background": "rgba(0,0,0,0)",
+	"max": "100"
+});
+var chatted = new ldBar('#chatted',{
+	"type": "fill",
+	"img": "assets/img/badges/chatted.svg",
+	"img-size": "100,100" ,
+	"fill-background": "rgba(0,0,0,0)",
+	"max": "100"
+});
+var shared = new ldBar('#shared',{
+	"type": "fill",
+	"img": "assets/img/badges/shared.svg",
+	"img-size": "100,100" ,
+	"fill-background": "rgba(0,0,0,0)",
+	"max": "100"
+});
+
+$('#fiveSeen').on('click', function(event) {
+	event.preventDefault();
+	for (var i = 0; i < 101; i++) {
+		fiveSeen.set(i);
+	}
+});
+
+$('#allSeen').on('click', function(event) {
+	event.preventDefault();
+	for (var i = 0; i < 101; i++) {
+		allSeen.set(i);
+	}
+});
+
+$('#profileSeen').on('click', function(event) {
+	event.preventDefault();
+	for (var i = 0; i < 101; i++) {
+		profileSeen.set(i);
+	}
+});
+
+$('#chatted').on('click', function(event) {
+	event.preventDefault();
+	for (var i = 0; i < 101; i++) {
+		chatted.set(i);
+	}
+});
+
+$('#shared').on('click', function(event) {
+	event.preventDefault();
+	for (var i = 0; i < 101; i++) {
+		shared.set(i);
+	}
+});
